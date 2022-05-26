39466b477a98d9d5a893c9640b5a07b168aa93e1
EmanuelConstanti <emanuel.constanti@hotmail.com>
2022-05-26 16:15:08 +0200

    initial commit

---
+++ src/main/java/ch/bbw/ec/SmartFarming/SmartFarming/SmartFarmingApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.ec.SmartFarming.SmartFarming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFarmingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartFarmingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/bbw/ec/SmartFarming/SmartFarming/SmartFarmingApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.ec.SmartFarming.SmartFarming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartFarmingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
